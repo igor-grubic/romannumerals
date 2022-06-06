f7630976f9195f9d9246bd5514c9dd7b3177842c
Igor Grubic <igor.grubic@westpac.co.nz>
2022-06-06 13:23:34 +1200

    supports number conversion up to 10

--- src/services/__tests__/romanNumerals.spec.js
+++ src/services/__tests__/romanNumerals.spec.js
@@ -6,6 +6,13 @@ describe('arabic numerals to roman numerals conversion', () => {
     ${1}   | ${'I'}
     ${2}   | ${'II'}
     ${3}   | ${'III'}
+    ${4}   | ${'IV'}
+    ${5}   | ${'V'}
+    ${6}   | ${'VI'}
+    ${7}   | ${'VII'}
+    ${8}   | ${'VIII'}
+    ${9}   | ${'IX'}
+    ${10}  | ${'X'}
   `('should return $roman when input is $arabic', ({arabic, roman}) => {
     expect(romanNumeralsService.convertFromArabic(arabic)).toBe(roman);
   });
--- src/services/romanNumerals.js
+++ src/services/romanNumerals.js
@@ -3,9 +3,45 @@ import romanSymbols from '../data/romanSymbols.js';
 const repeatSymbol = (symbol, times) =>
   [...Array(times)].reduce((previousValue) => `${previousValue}${symbol}`, '');
 
+const getRomanFromArabic = (arabicNumber) => {
+  const symbols = {
+    symbolForOne: romanSymbols.getSymbolFromArabic(1),
+    symbolForFive: romanSymbols.getSymbolFromArabic(5),
+    symbolForTen: romanSymbols.getSymbolFromArabic(10),
+  };
+
+  let result = '';
+  switch (arabicNumber) {
+    case 0:
+      return '';
+    case 1:
+    case 2:
+    case 3:
+      result = repeatSymbol(symbols.symbolForOne, arabicNumber);
+      break;
+    case 4:
+      result = `${symbols.symbolForOne}${symbols.symbolForFive}`;
+      break;
+    case 5:
+      result = symbols.symbolForFive;
+      break;
+    case 6:
+    case 7:
+    case 8:
+      result = `${symbols.symbolForFive}${repeatSymbol(symbols.symbolForOne, arabicNumber - 5)}`;
+      break;
+    case 9:
+      result = `${symbols.symbolForOne}${symbols.symbolForTen}`;
+      break;
+    case 10:
+      result = `${symbols.symbolForTen}`;
+      break;
+  }
+  return result;
+};
+
 const convertFromArabic = (number) => {
-  const symbolForOne = romanSymbols.getSymbolFromArabic(1);
-  return repeatSymbol(symbolForOne, number);
+  return getRomanFromArabic(number);
 };
 
 export default {
